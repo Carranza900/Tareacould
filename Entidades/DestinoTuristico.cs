d720847ad7896e27b525b68b73b90f0d362e010f
carranza900 <jaime.jose23901054@estu.unan.edu.ni>
2023-11-12 20:07:16 -0600

    Agregando la Entidad Destino Turistico

---
+++ Entidades/DestinoTuristico.cs
@@ -0,0 +1,15 @@
+program class CategoriaDestino
+{
+   public Guide Id { get; set; }
+
+   public Sring Nombre { get; set; } = string.Empty;
+
+   public String Descripcion { get; set; } = string.Empty;
+
+   public  String Ubicacion { get; set; } = string.Empty
+
+     void Crear() { }
+
+     void Eliminar() { }
+
+}
