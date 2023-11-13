172a16c4e49349fb0ae1980c987b9496b347bfd2
carranza900 <jaime.jose23901054@estu.unan.edu.ni>
2023-11-12 20:05:08 -0600

    Agregando la categoria EventoLocal

---
+++ Entidades/EventoLocal.cs
@@ -0,0 +1,17 @@
+program class CategoriaDestino
+{
+   public Guide Id { get; set; }
+
+   public Sring Nombre { get; set; } = string.Empty;
+
+   public  DateTime Fecha { get; set; } = MinValue;
+
+   public String Descripcion { get; set; } = string.Empty;
+
+   public  String Ubicacion { get; set; } = string.Empty
+
+     void Crear() { }
+
+     void Eliminar() { }
+
+}
