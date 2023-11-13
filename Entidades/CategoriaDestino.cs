3a55d2d9b852129eef489767ff86acbf396e36e2
carranza900 <jaime.jose23901054@estu.unan.edu.ni>
2023-11-12 20:08:24 -0600

    Agregando la Entidad Categoria Destino

---
+++ Entidades/CategoriaDestino.cs
@@ -0,0 +1,16 @@
+program class CategoriaDestino
+{
+   public Guide Id { get; set; }
+
+   public String Nombre{ get; set; } = string.Empty;
+
+   public String Descripcion { get; set; } = string.Empty;
+
+
+     void crear() { }
+
+     void Eliminar() { }
+
+
+
+}
